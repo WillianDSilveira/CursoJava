461d3ed22bfe8bad928f153b7ba3015c0c3f551b
Willian <williansilveira100@hotmail.com>
2023-11-01 12:35:43 -0300

    primeiro commit

---
+++ Modulo01/Aula01/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Oláaaaaaaaaaaaaaa, Mundo!");
+
+        // tipos primitivos
+        int idade = 28;
+        double altura = 1.84;
+        boolean casado = true;
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(casado);
+
+        // tipos wrapper
+
+        int idadeW = 28;
+        double alturaW = 1.84;
+        boolean casadoW = true;
+
+        final double PI = 3.14;
+
+        System.out.println(idadeW);
+        System.out.println(alturaW);
+        System.out.println(casadoW);
+
+    }
+}
---
+++ Modulo01/Aula01ex/src/Exercicio01.java
@@ -0,0 +1,14 @@
+public class Exercicio01 {
+    public static void main(String[] args){
+        double nota01 = 10;
+        double nota02 = 8;
+        final double PESO_NOTA = 0.5;
+
+        double CalculoAritmetico = (nota01 * PESO_NOTA) + (nota02 * PESO_NOTA);
+        
+        System.out.println(CalculoAritmetico);
+
+
+
+    }
+}
